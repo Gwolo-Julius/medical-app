f8fb479ac25d9147ca08cf7c3c7f61354e6fcaed
Julius <gwolojulius2@gmail.com>
2024-12-21 15:42:18 +0200

    My profile page is initiated

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
 
 const MyProfile = () => {
+  const [userData, setUserData] = useState({
+    name: "John",
+    image: assets.profile_pic,
+    email: "johndoe8@gmail.com",
+    phone: "+142 859 829 380",
+    address: {
+      line1: "57 cross, Liverpool",
+      line2: "Circle, Church road, London",
+    },
+    gender: "Male",
+    dob: "2000-10-20",
+  });
+  const [isEdit, setIsEdit] = useState(false);
   return (
-    <div className=''>
-      Profile page
+    <div className="">
+      <img src={userData.image} alt="" />
+      {userData.name}
     </div>
-  )
-}
+  );
+};
 
-export default MyProfile
+export default MyProfile;
