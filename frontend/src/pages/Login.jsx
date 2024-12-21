d8da97a480ce974b62ef4f66216a6cb8aa8d2219
Julius <gwolojulius2@gmail.com>
2024-12-21 14:19:09 +0200

    login page initiated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div className=''>
-      Login page
-    </div>
-  )
-}
+  const [state, setState] = useState("Sign Up");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
-export default Login
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form className="min-h-[80vh] flex flex-col">
+      <div className="flex flex-col gap-3  m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-lg shadow-lg text-sm text-zinc-600">
+        <p>{state === "Sign Up" ? "Create Account" : "Login"}</p>
+        <p>
+          Please {state === "Sign Up" ? "sign up" : "Log in"} to book for
+          appointment.
+        </p>
+        <div>
+          <label htmlFor="name">Full name:</label> <br />
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label> <br />
+          <input
+            type="text"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label> <br />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button>{state === "Sign Up" ? "Create Account" : "Login"}</button>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
